de32d5eb664940cdc45ca9d8cf4ce4e193e2a1b4
Márcio Conceição Goulart <marcio.goulart@gmail.com>
2018-07-26 20:28:46 -0300

    Testes incluídos

--- postagens/tests.py
+++ postagens/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Postagem
 
-# Create your tests here.
+
+class PostModelTeste(TestCase):
+    def setUp(self):
+        Postagem.objects.create(texto='texto de teste')
+
+    def teste_text_content(self):
+        postagem = Postagem.objects.get(id=1)
+     #   expected_object_name = f'{postagem.text}'
+     #   expected_object_name = '{' + postagem.texto + '}'
+        expected_object_name = postagem.texto
+        self.assertEqual(expected_object_name, 'texto de teste')
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Postagem.objects.create(texto='este é outro teste')
+
+    def teste_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def teste_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def teste_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
